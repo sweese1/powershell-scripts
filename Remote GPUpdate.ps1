30316434d065f8a5793812ba3e99075df156c6a7
Samuel Weese <147128443+sweese1@users.noreply.github.com>
2024-02-22 15:04:19 -0500

    Add files via upload

---
+++ Remote GPUpdate.ps1
@@ -0,0 +1,19 @@
+﻿#asking for PC Name to Query at beginning
+Write-host "Please enter the PC Name to be queried" -ForegroundColor Yellow
+$PCNAME = Read-Host "Please enter the PC Name"
+Write-Host "Thank You, Proceeding with the script" -ForegroundColor Green 
+
+#Executing turning on the Remote Protocol on the PC
+Write-Host "Turning on Remote Protocol" -ForegroundColor Yellow
+PsExec.exe /accepteula \\$PCNAME -h -s powershell.exe Enable-PSRemoting -Force
+Write-Host "Turning on Remote Protocol Completed, Proceeding with the remainder of the script" -ForegroundColor Green
+
+#command will force gpupdate using PsExec
+Write-Host "Running GPupdate Forced" -ForegroundColor Yellow
+PsExec.exe /accepteula \\$PCNAME gpupdate /force
+Write-Host "GPupdate Forced Completed" -ForegroundColor Green
+
+#turning off remote desktop protocol
+Write-Host "Turning off Remote Protocol" -ForegroundColor Yellow
+PsExec.exe /accepteula \\$PCNAME -h -s powershell.exe Disable-PSRemoting -Force
+Write-Host "Turning off Remote Protocol Completed" -ForegroundColor Green 
